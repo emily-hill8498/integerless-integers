d262168e0ad634feac3b6cfc33850df296594838
hill8498 <144700353+hill8498@users.noreply.github.com>
2024-01-24 10:14:31 -0500

    Update int.py

--- int.py
+++ int.py
@@ -2,28 +2,27 @@ sums = {}
 for i in range(0, 10):
     for j in range(0, 10):
         sums[str(i)+str(j)]=str(i+j)
+differences = {}
+for i in range(0, 10):
+    for j in range(0, 10):
+        differences[str(i)+str(j)]=str(i-j)
 # print(add)
 
 class int:
     def __init__(self, num):
         self.value = str(num)
+        self.negative = True if self.get_list.pop(False)=='-' else False
     def get_list(self):
         return list(self.value)
+    def get_sign(self):
+        return "negative" if self.negative else "positive"
     def add(self,  numObj):
+        if numObj.get_sign() == 'negative':
+            return self.sub(numObj)
+        elif self.get_sign() == 'negative':
+            return numObj.sub(self)
         val = self.get_list()
         num = numObj.get_list()
-        # valC = val.copy()
-        # numC = num.copy()
-        # bigger = '?'
-        # try:
-        #     while True:
-        #         valC.pop()
-        #         numC.pop()
-        # except IndexError:
-        #     if valC:
-        #         bigger = 'v'
-        #     else:
-        #         bigger = 'n'
         fsum = ''
         while val:
             try:
@@ -39,5 +38,7 @@ class int:
                   val.append(sum.pop())
         self.value =  fsum
         return fsum
+    def sub(self, numObj):
+        pass
 
 print(int('19').add(int('19')))
