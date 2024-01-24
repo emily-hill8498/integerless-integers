06767c6a95173883d152c953754545caa4491d4c
hill8498 <144700353+hill8498@users.noreply.github.com>
2024-01-24 09:49:11 -0500

    Create int.py
    Features:
    Number
    Addition

---
+++ int.py
@@ -0,0 +1,42 @@
+sums = {}
+for i in range(0, 10):
+    for j in range(0, 10):
+        sums[str(i)+str(j)]=str(i+j)
+# print(add)
+
+class int:
+    def __init__(self, num):
+        self.value = str(num)
+    def get_list(self):
+        return list(self.value)
+    def add(self,  numObj):
+        val = self.get_list()
+        num = numObj.get_list()
+        # valC = val.copy()
+        # numC = num.copy()
+        # bigger = '?'
+        # try:
+        #     while True:
+        #         valC.pop()
+        #         numC.pop()
+        # except IndexError:
+        #     if valC:
+        #         bigger = 'v'
+        #     else:
+        #         bigger = 'n'
+        fsum = ''
+        while val:
+            try:
+                sum = list(sums[num.pop()+val.pop()])
+            except IndexError:
+                sum = list(val.pop())
+            fsum = sum.pop()+fsum
+            print(fsum)
+            if sum:
+              try:
+                  val.append(str(int(val.pop()).add(int(sum.pop()))))
+              except IndexError:
+                  val.append(sum.pop())
+              print(fsum)
+
+int('9').add(int('9'))
